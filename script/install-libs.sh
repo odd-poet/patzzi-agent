73fb9f66c77f5d670a8e18029fe7d1a13b849f09
Yunsang Choi <oddpoet@gmail.com>
2013-12-30 02:34:37 +0900

    add argparse

--- script/install-libs.sh
+++ script/install-libs.sh
@@ -11,14 +11,16 @@ AGENT_HOME=$(cd $(dirname $0)/..;pwd)
 target_dir=$AGENT_HOME/lib
 function install() {
 	package_name=$1
-	pip install -t $target_dir -I --no-deps --install-option --prefix= \
+	pip install -t $target_dir -I --no-deps --force-reinstall --install-option --prefix= \
 		$package_name
 }
 
+rm -rf $target_dir
 mkdir -p $target_dir 
 
 echo "Install packages ..."
 echo "----------------------"
+install argparse
 install lockfile 
 install python-daemon
 install psutil 
